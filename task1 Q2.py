59c9e7efeeb7711526862ce3e2808717beb874ad
kotaruchik <59026156+kotaruchik@users.noreply.github.com>
2019-12-20 12:41:58 +0530

    Add files via upload

---
+++ task1 Q2.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+for i in range(1999,3201):
+    if(i%7==0 and i%5!=0):
+        print(i,end=",")
+
+
+# In[ ]:
+
+
+
+
---
+++ task1Q3.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+for i in range(1999,3201):
+    if(i%7==0 and i%5!=0):
+        print(i,end=",")
+
+
+# In[5]:
+
+
+x=input("Enter your first name : ")
+y=input("Enter your last name : ")
+print(y+" "+x)
+
+
+# In[ ]:
+
+
+
+
